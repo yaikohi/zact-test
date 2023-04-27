a30a60184f52431309a8cc1b52352c394f9092e7
yaikohi <erik.beem@hotmail.com>
2023-04-27 14:21:32 +0200

    ehe

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -8,6 +8,12 @@ const loginCreds = z.object({
   password: z.string().min(6),
 });
 
+/**
+ * For now only console logs to the server.
+ *
+ * The idea is that you would actually make a request
+ * to the server and generate a cookie or something similar.
+ */
 export const loginAction = zact(loginCreds)(async (input) => {
   console.log(loginCreds.parse(input));
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>hello!</h1>
+    <main className="flex flex-col gap-2">
+      <h1 className="text-4xl font-black tracking-widest uppercase">hello!</h1>
+      <Link className="px-3 py-2 rounded-xl bg-violet-400 " href="/users">
+        click violet colored element to see result
+      </Link>
     </main>
   );
 }
