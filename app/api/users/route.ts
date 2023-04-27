0d64fb2fec182ecbe360d071342743ff49e91d07
yaikohi <erik.beem@hotmail.com>
2023-04-27 14:06:56 +0200

    use json file instead for sake of testing i gess

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,6 @@
-export async function GET(request: Request) {
-  const res = await fetch("http://localhost:3000/users");
-  const data = (await res.json()).data;
-  return new Response(JSON.stringify(data, null, 2), {
+import dta from "../../../public/userdata.json";
+export async function GET(_: Request) {
+  return new Response(JSON.stringify(dta, null, 2), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
